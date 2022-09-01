e8143baa0e7930f49debc976e368e580daddd4df
Zac Smart <zac_chaz@outlook.com>
2022-09-01 22:56:25 +1000

    fixed syntax error had .getitem instead of .setitem in function and i had used the wrong storage key i used hour1 when it should of been hour-1, looks to be working correctly now

--- assets/script.js
+++ assets/script.js
@@ -11,12 +11,20 @@ saveBtn.forEach(function(node){
     node.addEventListener("click", function(){
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        localStorage.getItem(time, description);
+        localStorage.setItem(time, description);
     })
 });
 
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-6 .description").val(localStorage.getItem("hour-6"));
+$("#hour-7 .description").val(localStorage.getItem("hour-7"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
 
 var elements = $(".time-block");
 var d = new Date();
