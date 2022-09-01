08f31acb5b5a88314bc87517aa5c735b76f1f11a
Zac Smart <zac_chaz@outlook.com>
2022-09-01 17:13:14 +1000

    fixed colour blocking i had the data hour stored in text area not the div

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,50 @@
 const clock = document.getElementById("clock");
 setInterval(() => {
     const now = moment();
-    const readable =now.format("dddd, MMMM Do YYYY, h:mm:ss a");
+    const readable = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
     clock.textContent = readable;
-    
+
 }, 1000);
 
-const schedule = document.querySelector(".storage");
-const  saveBtn = document.querySelector(".button")
-const timeBlock = document.querySelector(".timeBlock")
+// const schedule = document.querySelector(".storage");
+// const  saveBtn = document.querySelector(".button")
+// const timeBlock = document.querySelector(".timeBlock")
 
-storageInput.addEventListener('input' , letter => {
-    text.textContent = letter.target.value
-} )
-
-
-
-const saveToStorage = ()=> {
-    localStorage.setItem('textinput', timeBlock.textContent)
-}
-
-button.addEventListener('click, saveToStorage')
+// storageInput.addEventListener('input' , letter => {
+//     text.textContent = letter.target.value
+// } )
 
 
-var elements = $("time-block");
+
+// const saveToStorage = ()=> {
+//     localStorage.setItem('textinput', timeBlock.textContent)
+// }
+
+// button.addEventListener('click, saveToStorage')
+
+
+var elements = $(".time-block");
 var d = new Date();
 var h = d.getHours();
-for (i=0; i < elements.length; i++){
-    var t = parseInt(elements[i].getAttribute(data-hour));
+for (i = 0; i < elements.length; i++) {
+    var t = parseInt(elements[i].getAttribute("data-hour"));
     if (h < t) {
         elements[i].classList.add("future");
         elements[i].classList.remove("past");
         elements[i].classList.remove("present");
-     } else if (h>t) {
-            elements[i].classList.add("past");
-            elements[i].classList.remove("future");
-            elements[i].classList.remove("present");
-      } else if (h===t){
+    } else if (h > t) {
+        elements[i].classList.add("past");
+        elements[i].classList.remove("future");
+        elements[i].classList.remove("present");
+    } else if (h === t) {
         elements[i].classList.add("present");
         elements[i].classList.remove("future");
         elements[i].classList.remove("past");
-      }else if (h>16&&h<9) {
-            elements[i].classList.add("future");
-            elements[i].classList.remove("past");
-            elements[i].classList.remove("present");
-        }
-      }
-          
-    
+    } else if (h > 16 && h < 9) {
+        elements[i].classList.add("future");
+        elements[i].classList.remove("past");
+        elements[i].classList.remove("present");
+    }
+}
+
+
