27b47cdf431a6c8d64df03f28a441cf1caaa0428
Zac Smart <zac_chaz@outlook.com>
2022-09-01 15:40:06 +1000

    updated timeblocks

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,18 @@ setInterval(() => {
     
 }, 1000);
 
+const schedule = document.querySelector(".storage");
+const  saveBtn = document.querySelector(".button")
+const timeBlock = document.querySelector(".timeBlock")
+
+storageInput.addEventListener('input' , letter => {
+    text.textContent = letter.target.value
+} )
 
 
 
+const saveToStorage = ()=> {
+    localStorage.setItem('textinput', timeBlock.textContent)
+}
 
+button.addEventListener('click, saveToStorage')
